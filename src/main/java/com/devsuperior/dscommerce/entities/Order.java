f999cb5241d141346945d23935f84afd1b4e68f6
Bruno Oliveiram <bruno.o.s@live.com>
2026-03-11 21:58:20 -0300

    Entidades Order, User e Enum OrderStatus

---
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -0,0 +1,57 @@
+package com.devsuperior.dscommerce.entities;
+import jakarta.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment, OrderStatus status, User client) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", moment=" + moment +
+                ", status=" + status +
+                ", client=" + client +
+                '}';
+    }
+
+}
