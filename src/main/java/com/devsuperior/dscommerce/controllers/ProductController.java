bcc57f231702f14fdb4f868b01481ae42a937b0f
Bruno Oliveiram <bruno.o.s@live.com>
2026-03-18 22:43:39 -0300

    Entidades Order, User, Enum OrderStatus, Payment e OrdemItem, 12/03 + seeding + Equals/HashCode

---
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscommerce.controllers;
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repository.ProductRepository;
+import com.devsuperior.dscommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id){
+        ProductDTO dto = service.findById(id);
+        return dto;
+    }
+
+}
